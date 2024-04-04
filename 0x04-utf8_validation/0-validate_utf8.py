3955c3916373171754fbd27df4ea2016dcdd3afd
Sharraff <111119433+Sharraff@users.noreply.github.com>
2024-04-04 20:39:51 +0000

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
 """
-check for unicode
+UTF-8 Validation
 """
 
 
-def validUTF8(data):
+def validUTF8(data) -> bool:
     """
-    validUTF8: checks for utf8 encoding
-    return: boolean
+    Returns True if data is a valid UTF-8 encoding, else return False
+    :param data:
+    :return:
     """
-    leader_count = 0
+    num_bytes = 0
     for byte in data:
-        byte = byte & 0xFF
-
-        if leader_count == 0:
-            if byte >> 7 == 0b0:
-                leader_count = 0
-            elif byte >> 5 == 0b110:
-                leader_count = 1
-            elif byte >> 4 == 0b1110:
-                leader_count = 2
-            elif byte >> 3 == 0b11110:
-                leader_count = 3
-            else:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            if byte >> 6 == 0b10:
-                leader_count -= 1
-            else:
+            if byte >> 6 != 0b10:
                 return False
-        return leader_count == 0
+        num_bytes -= 1
+    return num_bytes == 0
