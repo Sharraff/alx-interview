24261699c1ac50a0184068926959b6d9224e2a94
Sharraff <rogandsteinporter@gmail.com>
2024-09-03 10:12:18 -0700

    leetcode typeshit

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Make Change"""
+
+
+def makeChange(coins, total):
+    """Sort the coins and reduce the total based on the largest coin"""
+    coins.sort()
+    neededCoins = 0
+    while total > 0:
+        if coins:
+            coin = coins.pop()
+        else:
+            return -1
+        neededCoins += (total // coin)
+        total %= coin
+    return neededCoins
