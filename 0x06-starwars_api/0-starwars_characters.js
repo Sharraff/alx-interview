fdddad5a9c6d56ff672a24d64f22cb859605b6da
Sharraff <rogandsteinporter@gmail.com>
2024-09-05 06:57:54 -0700

    starwars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmId = process.argv[2];
+
+async function starwarsCharacters(filmId) {
+  const endpoint = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+  try {
+    const response = await request(endpoint);
+    const filmData = JSON.parse(response.body);
+    const characters = filmData.characters;
+
+    for (const characterUrl of characters) {
+      try {
+        const characterResponse = await request(characterUrl);
+        const characterData = JSON.parse(characterResponse.body);
+        console.log(characterData.name);
+      } catch (error) {
+        console.error('Error fetching character data:', error);
+      }
+    }
+  } catch (error) {
+    console.error('Error fetching film data:', error);
+  }
+}
+
+starwarsCharacters(filmId);
